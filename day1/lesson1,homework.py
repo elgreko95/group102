35cddd36072e12a26f71c6f76b4d91f412180ec1
elgreko95 <mishkamiish1995@gmail.com>
2023-03-03 00:06:44 +0400

    first commit

---
+++ day1/lesson1,homework.py
@@ -0,0 +1,6 @@
+name = "mikheil"
+surname = "nikolaevi"
+age = 22
+hight = 170.00
+
+print(name + " " + surname + " " + str(age) + " " + str(hight))
---
+++ day1/lesson1.py
@@ -0,0 +1,20 @@
+name = "mishka"
+
+age= 22
+
+surname = "nikolaevi"
+
+print(name)
+
+print(name + " " + surname)
+
+
+print(name + " " + surname)
+
+knows_programming = True
+
+is_ugly = False 
+
+print(name + " " + str(age))
+
+
