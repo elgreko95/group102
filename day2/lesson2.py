edef24709ab55a2c983a46aa348c3d3d70acaa47
elgreko95 <mishkamiish1995@gmail.com>
2023-03-03 12:02:46 +0400

    secod day commit

--- day2/lesson2.py
+++ day2/lesson2.py
@@ -1,1 +1,14 @@
-print("this is a lesson 2")
+name = "mikeil"
+surname = "nikolaevi"
+nikname = "elgreko"
+age = 27
+hight = 170.0
+is_thin = True
+knows_programming = False 
+is_laborious = True
+has_expierience = True
+knows_languages = True
+loves_reading = True
+is_wrking = True
+
+print(name +  " " + surname + " " + nikname + " " + str(age) + " " + str(hight)+ " " + str(is_thin) + " " + str(knows_programming) + " " + str(is_laborious) + " " + str(has_expierience) + " " + str(has_expierience) + " " + str(knows_languages) + " " + str(loves_reading) + " " + str(is_working))
