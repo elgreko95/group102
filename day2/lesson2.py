fa53abaacffd29e9e77c7a5413c4245036a0fb97
elgreko95 <mishkamiish1995@gmail.com>
2023-03-04 16:42:24 +0400

    day 3 commit

--- day2/lesson2.py
+++ day2/lesson2.py
@@ -9,6 +9,6 @@ is_laborious = True
 has_expierience = True
 knows_languages = True
 loves_reading = True
-is_wrking = True
+is_working = True
 
 print(name +  " " + surname + " " + nikname + " " + str(age) + " " + str(hight)+ " " + str(is_thin) + " " + str(knows_programming) + " " + str(is_laborious) + " " + str(has_expierience) + " " + str(has_expierience) + " " + str(knows_languages) + " " + str(loves_reading) + " " + str(is_working))
