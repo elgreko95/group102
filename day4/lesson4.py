9bfdf431d789dd0da5fc0f9b650103bed3309c3b
elgreko95 <mishkamiish1995@gmail.com>
2023-03-12 13:06:31 +0400

    day4 commit

---
+++ day4/lesson4.py
@@ -0,0 +1,96 @@
+# my_name = "mishka"
+
+# if "k" in my_name:
+#     print("sheicavs k-s")
+
+# my_name = "mishka"
+# my_surname = "nikolaevi"
+# my_age = 27
+# my_sentence = "aa {0} bb {2} cc {1}".format(my_name, my_surname, my_age)
+# print(my_sentence)
+
+
+# my_name = "mishka"
+# my_surname = "nikolaevi"
+# my_age = 27
+# hight = 170.00 
+# my_nikname = "elgreko"
+# is_working = True
+# is_laborious = True
+# my_sentence = "aa {0} bb {4} cc {1} dd {2} ee {3} ff {5} gg {6}".format(my_name, my_surname, my_age, hight, my_nikname, is_working, is_laborious)
+# print(my_sentence)
+
+# my_name = "mishka"
+
+# if "e" in my_name:
+#     print("sheicavs e-s")
+# else:
+#     print("ar sheicavs e-s")
+
+
+# input
+# output
+
+# my_name = input("enter your name: ")
+# print("chemi saxelia " + my_name)
+
+# my_age = 27 
+# my_age +=5
+# print(my_age)
+
+# my_age = input("enter your age: ")
+# my_name = input("enter your name: ")
+# my_surname = input("enter your surname: ")
+# print("my name is {}  my surname is {}  my age is {}".format(my_name, my_surname, my_age))
+
+# my_surname = input("enter your surname: ")
+# my_age = input("enter your age: ")
+# # my_name = input("enter your name: ")
+# # print("my name is {} my surname is {} my age is {}".format(my_name, my_surname, my_age))
+
+# year = input("enter years: ")
+
+# new_age = int(my_age) + int(year)
+# print("after {} years i am now {} years old".format(year, new_age))
+
+# first_number = int(input("enter first number: "))
+# second_number = int(input("enter second number: "))
+
+# product = first_number * second_number
+
+# if product > 100:
+#     print(product)
+# else:
+#     print("you lose")
+
+# input, %, if, += ინკრემენტაცია
+
+# print(10 / 3)
+# print(10 // 3)
+# print(5 ** 3)
+
+# მომხმარებელმა ტერმინალიდან შემოიტანოს 3 რიცხვი
+# აქედან მხოლოდ კენტები შეკრიბოს და დაპრინტოს ჯამი
+
+number1 = int(input("enter number1: "))
+number2 = int(input("enter number2: "))
+number3 = int(input("enter number3: "))
+
+# საინკრემენტაციო ცვლადი 
+ 
+my_sum = 0
+
+if number1 % 2 == 1:
+    my_sum += number1
+if number2 % 2 == 1:
+    my_sum += number2   
+if number3 % 2 == 1:
+    my_sum += number3
+
+print("The sum of odd numbers is {}".format(my_sum))                   
+
+
+
+
+
+
